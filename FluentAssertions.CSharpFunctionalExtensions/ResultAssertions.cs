8c6afbdd040afd459a4ab278ab2e1debc045ca51
Pedro <pedrotimoteocosta@gmail.com>
2020-02-14 21:34:05 +0100

    Add project files.

---
+++ FluentAssertions.CSharpFunctionalExtensions/ResultAssertions.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+using FluentAssertions.Execution;
+
+namespace FluentAssertions.CSharpFunctionalExtensions
+{
+    public class ResultAssertions
+    {
+        public Result Subject { get; set; }
+
+        public ResultAssertions(Result subject)
+        {
+            Subject = subject;
+        }
+
+        public AndConstraint<ResultAssertions> BeSuccess(string because = "", params object[] becauseArgs)
+        {
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(Subject.IsSuccess)
+                .FailWith("Expected {context:result} to be success, but it is failure. {0}", Subject);
+
+            return new AndConstraint<ResultAssertions>(this);
+        }
+    }
+}
