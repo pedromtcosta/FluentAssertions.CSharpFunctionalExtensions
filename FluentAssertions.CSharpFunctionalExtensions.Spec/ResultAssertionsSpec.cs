980f314af05e7fa2a51c517258ee9739f9ade15a
Pedro <pedrotimoteocosta@gmail.com>
2020-02-14 21:55:42 +0100

    Add unit test project

---
+++ FluentAssertions.CSharpFunctionalExtensions.Spec/ResultAssertionsSpec.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FluentAssertions.CSharpFunctionalExtensions.Spec
+{
+    public class ResultAssertionsSpec
+    {
+        [Fact]
+        public void Dummy()
+        {
+            Assert.True(true);
+        }
+    }
+}
